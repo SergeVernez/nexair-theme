acda74ee94c6037de3fd72937e1ac88963044b08
SergeVernez <sergevernez.pro@gmail.com>
2022-10-14 15:54:52 +0200

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Nexair
+ */
+
+get_header();
+?>
+
+	<main>
+			<!----- début de section accueil ----->
+			<section class="bg-home">
+				<div class="bg-black">
+					<div class="container">
+						<!----- début TXT+CTA ----->
+						<div class="w50">
+							<h1>ingénierie traitement de l'air</h1>
+							<h2>30 ans d’expérience...</h2>
+							<p><strong>...Basée dans le Nord de la France à Templemars</strong>, Nexair vous accompagne, de l’étude à la mise en oeuvre de vos installations, pour tout projet de <strong>dépoussiérage, ventilation, transport pneumatique ou nettoyage centralisé,</strong> avec un seul et même objectif: <strong> fournir une solution sur mesure et un accompagnement sur le long terme.</strong></p>
+							<a href="#" class="btn">Voir nos réalisations</a>
+							<h2>Audit - étude - conseil</h2>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!----- début section secteur ----->
+			<section>
+				<div class="container">
+					<h2>secteurs d'activités</h2>
+				</div>
+			</section>
+
+			<section class="container activites">
+				<div class="bloc-secteur agroalimentaire">
+					<h3 class="btn">Agroalimentaire</h3>
+					<div class="bloc-contenu">
+						<p>Agro, Céréales, Semences, Sucre</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur mineral">
+					<h3 class="btn">mineral</h3>
+					<div class="bloc-contenu">
+						<p>Mines, Verreries, Cimenteries, Carrières</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur recyclage">
+					<h3 class="btn">recyclage</h3>
+					<div class="bloc-contenu">
+						<p>Déchets, Centres de tri</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur caoutchouc">
+					<h3 class="btn">caoutchouc</h3>
+					<div class="bloc-contenu">
+						<p>Agents de Vulcanisation, Solvants, Antioxydants et Polluants chimiques</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur bois">
+					<h3 class="btn">bois</h3>
+					<div class="bloc-contenu">
+						<p>Menuiseries, scieries, biomasse</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur mettalurgie">
+					<h3 class="btn">mettalurgie</h3>
+					<div class="bloc-contenu">
+						<p>Fonderie, Métallurgie, Sidérurgie</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur papier">
+					<h3 class="btn">papier / carton / textile</h3>
+					<div class="bloc-contenu">
+						<p>Papeteries, Cartonneries, Fabricants de papier, Imprimeries, Industrie textile</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur chimie">
+					<h3 class="btn">chimie / engrais / pharmacie</h3>
+					<div class="bloc-contenu">
+						<p>Filtration chimie, Extraction des vapeurs, Ventilation des ateliers, Environnement des ATEX, Récupération des calories</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+
+				<div class="bloc-secteur auto">
+					<h3 class="btn">automobile / aéronautique</h3>
+					<div class="bloc-contenu">
+						<p>Ventilation, Nettoyage Centralisé, Extraction, Aspiration de brouillards d'huile, Captage des polluants</p>
+						<a href="#" class="btn">En savoir +</a>
+					</div>
+				</div>
+			</section>
+
+			<!----- début section Atex ----->
+			<section class="atex">
+				<div class="container">
+					<div>
+						<h2>spécification atex</h2>
+						<p>La prévention des risques d’explosion sur les lieux de travail fait l’objet d’une réglementation spécifique, directive ATEX 2014/34/UE. L’application des principes généraux de la prévention et de l’évaluation du risque d’explosion est une obligation de l’employeur.</p>
+						<p>De part son expérience en atmosphère explosive (ATEX), notre société vous apporte un soutien dans votre démarche de mise en conformité aux directives européennes :</p>
+						<p>Utilisateurs 1999/92/CE, applicable au 30/06/2006. Constructeurs 94/9/CE applicable depuis le 30/06/2003.</p>
+						<p>Nous vous proposons de <strong>modifier et adapter vos installations</strong> non-conformes.</p>
+						<a class="btn" href="#">contactez-moi</a>
+					</div>
+					<div>
+						<img src="/img/img atex copie.png" alt="" />
+					</div>
+				</div>
+			</section>
+
+			<!----- début section Savoir faire ----->
+			<section class="savoirFaire">
+				<div class="container">
+					<h2>Notre savoir faire</h2>
+
+					<div class="bloc-savoirfaire">
+						<h3>aspiration et dépousiérage industriel</h3>
+						<p>Pour évacuer de façon continue et centralisée toute pollution de l’air intérieur</p>
+						<a href="#" class="btn">en savoir +</a>
+					</div>
+
+					<div class="bloc-savoirfaire">
+						<h3>Nettoyage centralisé</h3>
+						<p>Pour un air propre</p>
+						<a href="#" class="btn">en savoir +</a>
+					</div>
+
+					<div class="bloc-savoirfaire">
+						<h3>ventillation industrielle</h3>
+						<p>Pour un air propre et renouvelé</p>
+						<a href="#" class="btn">en savoir +</a>
+					</div>
+
+					<div class="bloc-savoirfaire">
+						<h3>transport pneumatique</h3>
+						<p>Pour transporter vrac, poudres, granulés…</p>
+						<a href="#" class="btn">en savoir +</a>
+					</div>
+
+					<div class="bloc-savoirfaire">
+						<h3>transport de déchets</h3>
+						<p>Pour supprimer le ramassage manuel et optimiser la récupération de matières</p>
+						<a href="#" class="btn">en savoir +</a>
+					</div>
+
+					<div class="bloc-savoirfaire">
+						<h3>récupération d’energie</h3>
+						<p>Le recyclage d’air</p>
+						<a href="#" class="btn">en savoir +</a>
+					</div>
+				</div>
+			</section>
+
+			<!----- début section produits ----->
+			<section class="produits">
+				<div class="container">
+					<h2>Nos produits</h2>
+					<div>
+						<img src="#" alt="" />
+						<h3></h3>
+						<p></p>
+					</div>
+					<div>
+						<img src="#" alt="" />
+						<h3></h3>
+						<p></p>
+					</div>
+					<a href="#" class="btn">voir tous nos produits</a>
+				</div>
+			</section>
+
+			<!----- début section réalisation/galerie ----->
+			<section class="realisation">
+				<div class="container">
+					<h2>Nos réalisations</h2>
+					<a href="#" class="btn">voir +</a>
+				</div>
+			</section>
+		</main>
+
+<?php
+get_footer();
---
+++ header.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Nexair
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header class="container">
+			<div class="logo">
+				<a href="#">
+					<img src="img/Logo.svg" alt="logo Nexair" />
+				</a>
+			</div>
+
+			<!----- menu desktop ------>
+			<nav class="menu">
+				<ul>
+					<li><a href="#">secteur</a></li>
+					<li><a href="#">savoir-faire</a></li>
+					<li><a href="#">produits</a></li>
+					<li><a href="#">services</a></li>
+					<li><a href="#">réalisations</a></li>
+				</ul>
+			</nav>
+
+			<!------------------ menu formulaire de contact ---------------->
+			<nav class="menucontact">
+				<ul>
+					<li><a href="#">demande de rappel</a></li>
+					<li><a href="#">demander un devis</a></li>
+				</ul>
+			</nav>
+			<div class="projet">
+				<h4>Votre projet</h4>
+				<form action="">
+					<label class="w100">
+						Nom
+						<input type="text" name="" id="" />
+					</label>
+					<label class="w100">
+						Prénom
+						<input type="text" name="" id="" />
+					</label>
+					<label class="w100">
+						Email
+						<input type="text" name="" id="" /> </label
+					><label class="w100">
+						Variable
+						<input type="text" name="" id="" />
+					</label>
+					<label class="w50">
+						Champ 1
+						<input type="text" name="" id="" />
+					</label>
+					<label class="w50">
+						Champ 2
+						<input type="text" name="" id="" />
+					</label>
+					<label class="w50">
+						Champ 3
+						<input type="text" name="" id="" />
+					</label>
+					<label class="w50">
+						Champ 4
+						<input type="text" name="" id="" />
+					</label>
+					<input type="submit" value="Envoyer" class="btn" />
+				</form>
+			</div>
+		</header>
