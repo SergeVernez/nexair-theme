f9ca92a920236b03254a4fab314cdcc3333a84ca
SergeVernez <sergevernez.pro@gmail.com>
2022-11-30 11:00:54 +0100

    modif footer

--- footer.php
+++ footer.php
@@ -12,7 +12,53 @@
 ?>
 
 		<footer class="bg-footer">
-			<img src="/img/Logo.svg" alt="" />
+			<div class="container">
+				<img src="img/NEXAIR BLANC+BASE LINE.svg" alt="nexair ingenerie traitement de l'air" />
+				<nav>
+					<ul>
+						<li><a href="#">Notre savoir-faire</a></li>
+						<li><a href="#">Secteurs d'activités</a></li>
+						<li><a href="#">Nos produits</a></li>
+						<li><a href="#">Nos réalisations</a></li>
+						<li><a href="#">Nos services</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+
+				<hr />
+				<div class="ligne">
+					<div>
+						<h3>Siège Social</h3>
+						<p>
+							15E rue du Plouvier <br />
+							59175 - TEMPLEMARS <br />
+							+33 (0)3 20 35 89 54 <br />
+							+33 (0)3 20 35 94 03
+							<br />
+							<a href="#">contact[at]nexair.fr</a>
+						</p>
+					</div>
+
+					<div>
+						<h3>Agence de Lyon</h3>
+						<p>
+							6 rue de Savoie <br />
+							38070 - SAINT QUENTIN FALLAVIER <br />
+							+33 (0)4 74 95 00 12 +33 <br />
+							(0)4 74 94 08 51
+						</p>
+					</div>
+				</div>
+				<hr class="ligne" />
+				<div class="d-f">
+					<div><a href="#">Mentions légales</a></div>
+					<div>
+						<a href="#"> <i class="fa-brands fa-linkedin"></i></a>
+					</div>
+					<div><p>2022 - NEXAIR - Site réalisé par SLV</p></div>
+				</div>
+				div
+			</div>
 		</footer>
 <?php wp_footer(); ?>
 
