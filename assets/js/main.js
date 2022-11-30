4296daf9e2bc6e52aeca38ea166608b9032b6f31
SergeVernez <sergevernez.pro@gmail.com>
2022-11-30 12:02:07 +0100

    slider compact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
 jQuery(document).ready(function ($) {
 	$("#unite").unitegallery({
-		slider_enable_text_panel:true,
-        strippanel_enable_handle:false,
-        slider_textpanel_enable_title:false,
-        textpanel_enable_bg:false,
-        slider_textpanel_css_title:[object Object],
-        slider_textpanel_css_description:[object Object],
-        slider_textpanel_padding_bottom:15,
+		gallery_theme: "compact",
 	});
 });
