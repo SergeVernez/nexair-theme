03ca8868cce8a93062caead0035634d0df7d28c1
SergeVernez <sergevernez.pro@gmail.com>
2022-12-08 11:26:54 +0100

    test activité

--- single-secteur_activite.php
+++ single-secteur_activite.php
@@ -12,6 +12,26 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+	    <!----- début de section accueil ----->
+    <section class="bg-home">
+        <div class="bg-black">
+            <div class="container">
+                <!----- début TXT+CTA ----->
+                <div class="w50">
+                    <h1>ingénierie traitement de l'air</h1>
+                    <h2>20 ans d’expérience...</h2>
+                    <p><strong>...Basée dans le Nord de la France à Templemars</strong>,<br>Nexair vous accompagne, de
+                        l’étude à la mise en oeuvre de vos installations, pour tout projet de <strong>dépoussiérage,
+                            ventilation, transport pneumatique ou nettoyage centralisé,</strong> avec un seul et même
+                        objectif : <strong> fournir une solution sur mesure et un accompagnement<br> sur le long
+                            terme.</strong></p>
+                    <a href="#" class="btn">Voir nos réalisations</a>
+                    <h2>Audit - étude - conseil</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <?php the_content() ?>
 
 	</main><!-- #main -->
