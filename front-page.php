14b6495ad7b5990017ccf2b4f0bf68aaa3747dca
SergeVernez <sergevernez.pro@gmail.com>
2022-12-09 11:05:15 +0100

    Jean-mich

--- front-page.php
+++ front-page.php
@@ -88,7 +88,7 @@ get_header();
                 <a class="btn" href="#">contactez-moi</a>
             </div>
             <div>
-                <img src="/img/img atex copie.png" alt="" />
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/img atex copie.png" alt="" />
             </div>
         </div>
     </section>
