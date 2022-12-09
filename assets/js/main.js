d3dfde077b5d2ff90ba2c190c9acbfa3ce70ed95
SergeVernez <sergevernez.pro@gmail.com>
2022-12-09 14:52:56 +0100

    Fix appel JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,8 @@ jQuery(document).ready(function ($) {
 		strip_space_between_thumbs: 15,
 		gallery_height: 800,
 	});
-	$(".menucontact").click(function(){
-		$('.projet').toggleClass('ouvert');
+	$(".menucontact").click(function () {
+		$(this).toggleClass("ouvert");
+		$(".projet").toggleClass("ouvert");
 	});
 });
