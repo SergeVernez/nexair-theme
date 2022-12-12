1f5329c993a6af1e8ea53f9deb04e6f422a8a31e
SergeVernez <sergevernez.pro@gmail.com>
2022-12-12 10:23:38 +0100

    test media galerie JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,32 @@
 jQuery(document).ready(function ($) {
-	$("#unite").unitegallery({
-		gallery_theme: "compact",
-		slider_enable_text_panel: true,
-		strippanel_enable_handle: false,
-		strippanel_background_color: "transparent",
-		thumb_round_corners_radius: 15,
-		thumb_height: 120,
-		thumb_fixed_size: false,
-		strip_space_between_thumbs: 15,
-		gallery_min_height: 800,
-		gallery_width: 3000,
-	});
+	if ($(window).width() < 767) {
+		$("#unite").unitegallery({
+			gallery_theme: "compact",
+			slider_enable_text_panel: true,
+			strippanel_enable_handle: false,
+			strippanel_background_color: "transparent",
+			thumb_round_corners_radius: 15,
+			thumb_height: 120,
+			thumb_fixed_size: false,
+			strip_space_between_thumbs: 15,
+			gallery_min_height: 400,
+			gallery_width: 3000,
+		});
+	} else {
+		$("#unite").unitegallery({
+			gallery_theme: "compact",
+			slider_enable_text_panel: true,
+			strippanel_enable_handle: false,
+			strippanel_background_color: "transparent",
+			thumb_round_corners_radius: 15,
+			thumb_height: 120,
+			thumb_fixed_size: false,
+			strip_space_between_thumbs: 15,
+			gallery_min_height: 800,
+			gallery_width: 3000,
+		});
+	}
+
 	$(".menucontact,.btnContact").click(function () {
 		$(".menucontact,.projet").toggleClass("ouvert");
 	});
