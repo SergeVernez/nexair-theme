1e483e820e2efc25de0105a05bb0c01c064cbeb9
SergeVernez <sergevernez.pro@gmail.com>
2022-12-12 13:28:04 +0100

    fiche produit

--- single-produit.php
+++ single-produit.php
@@ -30,8 +30,8 @@ get_header();
 			<h2>Avantages</h2>
 			<?php the_field("avantage") ?>
 			<div class="liens">
-				<a href="<?php the_field("fiche") ?>" class="btn">fiche</a>
-				<a href="<?php echo home_url() ?>/contact" class="btn">contact</a>
+				<a href="<?php the_field("fiche_produit") ?>" class="btn" download>fiche</a>
+				<a href="<?php echo home_url() ?>/contact" class="btn btnContact">contact</a>
 			</div>
 		</div>
 		<div>
