50cead247656c10b45aa1e9b952448c21736902a
SergeVernez <sergevernez.pro@gmail.com>
2022-12-13 15:20:24 +0100

    buurger

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <header class="container">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/Vector burger.svg" alt="">
         <div class="logo">
             <?php the_custom_logo() ?>
         </div>
