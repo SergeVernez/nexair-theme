878a7779725b1d0bb44f4d00bd9f89a10d285b20
SergeVernez <sergevernez.pro@gmail.com>
2022-12-14 09:28:27 +0100

    phone

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
         <nav class="menucontact">
             <ul>
                 <li><a href="">contact</a></li>
-                <img src="<?php echo get_template_directory_uri() ?>/assets/img/Vector retour.svg" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/Vector phone.svg" alt="">
             </ul>
         </nav>
         <div class="projet">
