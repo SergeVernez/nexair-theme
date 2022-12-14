bf01fc7e18d858e4346e83b4a1565c99f7b53d92
SergeVernez <sergevernez.pro@gmail.com>
2022-12-14 09:58:02 +0100

    test phone

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
         <!------------------ menu formulaire de contact ---------------->
         <nav class="menucontact">
             <ul>
-                <li><a href="">contact</a></li>
+                <li><a href=""> <img class="phone" src="<?php echo get_template_directory_uri() ?>/assets/img/Vector phone.svg" alt=""> contact</a></li>
                 <img class="phone" src="<?php echo get_template_directory_uri() ?>/assets/img/Vector phone.svg" alt="">
             </ul>
         </nav>
